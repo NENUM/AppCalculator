1e5a44172affccb0fd197dca69bd3115fbebbfa7
diego.alejandro.mahecha.r@gmail.com <diego.alejandro.mahecha.r@gmail.com>
2023-01-23 16:14:41 -0500

    Agregar archivos de proyecto.

---
+++ App Calculator/ArithmeticLogic.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App_Calculator
+{
+    internal class ArithmeticLogic
+    {
+        private decimal a;
+        private decimal b;
+
+        public bool ConvertNumber(string num)
+        {
+            try
+            {
+                Double.Parse(num);
+            }
+            catch (Exception e)
+            {
+                return false;
+            }
+            return true;
+        }
+        public bool ConvertNumber(string num1, string num2)
+        {
+            try
+            {
+                a = Decimal.Parse(num1);
+                b = Decimal.Parse(num2);
+            }
+            catch (Exception e)
+            {
+                return false;
+            }
+            return true;
+        }
+        public decimal Addition()
+        {
+            return a + b;
+        }
+
+        public decimal Subtraction()
+        {
+            return a - b;
+        }
+
+        public decimal Multiplication()
+        {
+            return a * b;
+        }
+
+        public string Division()
+        {
+            decimal result; 
+            try
+            {
+                result = a / b;
+                return result.ToString();
+            }
+            catch(Exception e)
+            {
+                return $"Error, {e.Message}";
+            }
+            
+        }
+
+        public string Modulo()
+        {
+            decimal result;
+            try
+            {
+                result = a / b;
+                return result.ToString();
+            }
+            catch (Exception e)
+            {
+                return $"Error, {e.Message}";
+            }
+        }
+    }
+}
---
+++ App Calculator/Program.cs
@@ -0,0 +1,49 @@
+﻿using App_Calculator;
+
+ArithmeticLogic arithmeticLogic = new ArithmeticLogic();
+bool condition = true;
+string option;
+string a;
+string b;
+
+Console.WriteLine("Welcome to App calculator\n");
+Console.WriteLine("Please choose one option of the list\n");
+
+while (condition)
+{
+    Console.WriteLine("Enter first number: ");
+    a = Console.ReadLine();
+    if (!arithmeticLogic.ConvertNumber(a)) { Console.WriteLine("Please write a valid number\n"); continue; }
+    Console.WriteLine("Enter second number: ");
+    b = Console.ReadLine();
+    if (!arithmeticLogic.ConvertNumber(b)) { Console.WriteLine("Please write a valid number\n"); continue; }
+    arithmeticLogic.ConvertNumber(a, b);
+
+    Console.WriteLine("Arithmetic list operations: \n1. A - addition. \n2. S - subtraction. \n3. M - multiplication. \n4. D - division. \n5.Mo - modulo. \nE - Exit.\n");
+    option = Console.ReadLine();
+
+    switch (option)
+    {
+        case "A":
+            Console.WriteLine($"The answer is: {arithmeticLogic.Addition()}\n");
+            break;
+        case "S":
+            Console.WriteLine($"The answer is: {arithmeticLogic.Subtraction()}\n");
+            break;
+        case "M":
+            Console.WriteLine($"The answer is: {arithmeticLogic.Multiplication()}\n");
+            break;
+        case "D":
+            Console.WriteLine($"The answer is: {arithmeticLogic.Division()}\n");
+            break;
+        case "Mo":
+            Console.WriteLine($"The answer is: {arithmeticLogic.Modulo()}\n");
+            break;
+        case "E":
+            condition = false;
+            break;
+        default:
+            Console.WriteLine("You choose a wrong option, please try again.");
+            break;
+    }
+}
